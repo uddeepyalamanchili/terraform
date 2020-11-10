38e9badd372ab94425a0515e0c8a0dc95bfbf2f1
Uddip Yalamanchili <uddeepyalamanchili@gmail.com>
2020-11-10 13:06:56 +0530

    Adding the Full ELB code

--- assignment_1.tf
+++ assignment_1.tf
@@ -108,3 +108,22 @@ resource "aws_elb" "bar" {
     lb_protocol        = "https"
     ssl_certificate_id = "arn:aws:iam::123456789012:server-certificate/certName"
   }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8000/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.foo.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "foobar-terraform-elb"
+  }
+}
+
